6bc3cf65f7440bf53bcc91227409204b1aae43f1
Oliver Kofoed <git@oliverkofoed.com>
2011-02-10 01:06:21 +0100

    Changed to Markdown for readme and added download files

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChromeReloader")]
+[assembly: AssemblyTitle("Chrome RapidReload")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ChromeReloader")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chrome RapidReload")]
+[assembly: AssemblyCopyright("Oliver Kofoed, 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
